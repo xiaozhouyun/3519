4f19d4b4ea35aaf4ce29d15938a74ae3cbfd54dc
wsnd-die <3503707026@qq.com>
2026-07-29 00:30:45 +0800

    垃圾

--- keil/Objects/main.d
+++ keil/Objects/main.d
@@ -93,15 +93,10 @@
   D:\ti\mspm0_sdk_2_11_00_07\source\ti\driverlib\m0p\dl_interrupt.h \
   D:\ti\mspm0_sdk_2_11_00_07\source\ti\driverlib\m0p\dl_systick.h \
   ..\core\inc\zf_device_tft180.h ..\core\inc\icm42688p.h \
-<<<<<<< HEAD
   ..\core\inc\fast_math.h ..\core\inc\Grayscale.h \
   D:\TI\ KEIL538\ARM\ARMCLANG\Bin\..\include\string.h \
   ..\core\inc\encode.h ..\core\inc\drv8873.h ..\core\inc\blue.h \
   ..\core\inc\follow_line.h ..\core\inc\MG513XGMR.h ..\core\inc\Q_pid.h \
-=======
-  ..\core\inc\fast_math.h ..\core\inc\Grayscale.h ..\core\inc\encode.h \
-  ..\core\inc\drv8873.h ..\core\inc\blue.h ..\core\inc\follow_line.h \
->>>>>>> 24f14203210807122c43f4667c79981b06e2a290
   ..\freertos\Source\include\FreeRTOS.h \
   ..\freertos\builds\LP_MSPM0G3507\release\FreeRTOSConfig.h \
   ..\freertos\Source\include\projdefs.h \
